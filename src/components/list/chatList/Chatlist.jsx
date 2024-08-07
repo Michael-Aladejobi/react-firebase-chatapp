a6a0981b771be35eb659533f875e08a7e0901cc3
Michael-Aladejobi <mikkystechs@gmail.com>
2024-08-07 14:58:31 +0100

    updates

--- src/components/list/chatList/Chatlist.jsx
+++ src/components/list/chatList/Chatlist.jsx
@@ -1,7 +1,64 @@
 import "./chatlist.css";
+import { useState } from "react";
 
 const Chatlist = () => {
-    return <div className="chatlist">Chatlist</div>;
+    const [addMode, setAddMode] = useState(false);
+    return (
+        <div className="chatlist">
+            <div className="search">
+                <div className="searchBar">
+                    <img src="./search.png" alt="" />
+                    <input type="text" placeholder="search" />
+                </div>
+                <img
+                    src={addMode ? "./minus.png" : "./plus.png"}
+                    alt=""
+                    className="add"
+                    onClick={() => setAddMode((prev) => !prev)}
+                />
+            </div>
+
+            <div className="item">
+                <img src="avatar.png" alt="" />
+                <div className="texts">
+                    <span>Michael A</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="avatar.png" alt="" />
+                <div className="texts">
+                    <span>Michael A</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="avatar.png" alt="" />
+                <div className="texts">
+                    <span>Michael A</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="avatar.png" alt="" />
+                <div className="texts">
+                    <span>Michael A</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+
+            <div className="item">
+                <img src="avatar.png" alt="" />
+                <div className="texts">
+                    <span>Michael A</span>
+                    <p>Hello</p>
+                </div>
+            </div>
+        </div>
+    );
 };
 
 export default Chatlist;
--- src/components/list/userInfo/Userinfo.jsx
+++ src/components/list/userInfo/Userinfo.jsx
@@ -4,7 +4,7 @@ const Userinfo = () => {
         <div className="userinfo">
             <div className="user">
                 <img src="./avatar.png" alt="" />
-                <h2>Michael A</h2>
+                <h2>Michael A.</h2>
             </div>
             <div className="icons">
                 <img src="./more.png" alt="" />
